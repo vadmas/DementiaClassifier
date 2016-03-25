9eaa833717eadaf5fd8a228817e3a31d612d1a7e
Vaden Masrani <vadmas@gmail.com>
2016-03-24 18:12:03 -0700

    Made skeleton

---
+++ Driver.py
@@ -0,0 +1,11 @@
+import extractor.py
+import parser.py
+
+
+if __name__ == '__main__':
+	pass
+	# 1. parse arguments
+	# 2. Call parser to make list of strings
+	# 3. Call extractor to make list of features
+	# 4. Make arff file
+	# 5. export / call weka to train
