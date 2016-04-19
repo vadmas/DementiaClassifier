cca3a931fb9664d47c8007c6421398fd9c8dabd9
Halldor Thorhallsson <halldorbjarni@gmail.com>
2016-04-18 23:04:20 -0700

    Run experiments completed

--- run_experiments.sh
+++ run_experiments.sh
@@ -7,30 +7,30 @@ function runclassifiers() {
 	
 	cd logistic_regression/
 
-	java -cp "./weka.jar" weka.classifiers.functions.Logistic -t ../arff/$1_$2.arff > results.txt
+	java -cp "./../../../../weka.jar" weka.classifiers.functions.Logistic -t ../../../../arff_files/$1_$2.arff > results.txt
 
 	cd ../naive_bayes
 
-	java -cp "./weka.jar" weka.classifiers.bayes.NaiveBayes -t ../arff/$1_$2.arff > results.txt
+	java -cp "./../../../../weka.jar"  weka.classifiers.bayes.NaiveBayes -t ../../../../arff_files/$1_$2.arff > results.txt
 
 
 	cd ../neuralnets
 
-	java -cp "./weka.jar" weka.classifiers.functions.MultilayerPerceptron -t ../arff/$1_$2.arff > results.txt
+	java -cp "./../../../../weka.jar"  weka.classifiers.functions.MultilayerPerceptron -t ../../../../arff_files/$1_$2.arff > results.txt
 
 
 	cd ../random_forest
 
-	java -cp "./weka.jar" weka.classifiers.trees.RandomForest -t ../arff/$1_$2.arff > results.txt
+	java -cp "./../../../../weka.jar"  weka.classifiers.trees.RandomForest -t ../../../../arff_files/$1_$2.arff > results.txt
 
 
 	cd ../svm
 
-	java -cp "./weka.jar" weka.classifiers.functions.SMO-t ../arff/$1_$2.arff > results.txt
+	java -cp "./../../../../weka.jar"  weka.classifiers.functions.SMO -t ../../../../arff_files/$1_$2.arff > results.txt
 
 	cd ../bayesnet
 
-	java -cp "./weka.jar"  weka.classifiers.bayes.BayesNet-t ../arff/$1_$2.arff > results.txt
+	java -cp "./../../../../weka.jar"   weka.classifiers.bayes.BayesNet -t ../../../../arff_files/$1_$2.arff > results.txt
 
 
 	cd ../
