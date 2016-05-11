24c5d4a14c5bc7c8bf43b6132bf1e19ef8610fb9
Vaden Masrani <vadmas@gmail.com>
2016-05-11 15:53:22 -0700

    Moved all the WEKAFormatter logic into driver

--- Driver.py
+++ Driver.py
@@ -8,9 +8,10 @@ from FeatureExtractor import parser
 from FeatureExtractor import pos_phrases 
 from FeatureExtractor import pos_syntactic 
 from FeatureExtractor import psycholinguistic
-import random
+from random import shuffle
 
 # constants
+
 # Unsplit data
 DEMENTIABANK_CONTROL_DIR_ALL  = 'data/dbank/control'
 DEMENTIABANK_DEMENTIA_DIR_ALL = 'data/dbank/dementia'
@@ -39,8 +40,11 @@ PICKLE_DIR  = 'data/pickles/'
 #PICKLE_DIR = 'stanford/processed/pickles/'
 TEST_DIR    = 'data/test/'
 
-#Output Directory
-OUTPUT_DIR = "FeatureVecs/"
+#Processed feature vector directory
+FEATURE_VEC_DIR = "FeatureVecs/"
+
+#Arff file directory
+ARFF_DIR = 'arff files/'
 
 #Book lines / interview
 BOOK_LINES_PER_INTERVIEW = 5
@@ -85,42 +89,10 @@ def get_book_data(picklename, raw_files_directory):
         save_pickle(PICKLE_DIR + picklename,data)
     return data
 
-#dataset = all, train, or test
-def get_data(dataset):
-    dataset = dataset.lower()
-    if dataset == 'train':
-        dbank_control_train  = read_file('dbank_control_train.pickle', DEMENTIABANK_CONTROL_DIR_TRAIN)
-        dbank_dem_train      = read_file('dbank_dem_train.pickle',     DEMENTIABANK_DEMENTIA_DIR_TRAIN)
-        optima_control_train = read_file('optima_control_train.pickle',OPTIMA_CONTROL_DIR_TRAIN)
-        optima_dem_train     = read_file('optima_dem_train.pickle',    OPTIMA_DEMENTIA_DIR_TRAIN)
-        return dbank_control_train, dbank_dem_train, optima_control_train, optima_dem_train
-    elif dataset == 'test':
-        dbank_control_test  = read_file('dbank_control_test.pickle', DEMENTIABANK_CONTROL_DIR_TEST)
-        dbank_dem_test      = read_file('dbank_dem_test.pickle',     DEMENTIABANK_DEMENTIA_DIR_TEST)
-        optima_control_test = read_file('optima_control_test.pickle',OPTIMA_CONTROL_DIR_TEST)
-        optima_dem_test     = read_file('optima_dem_test.pickle',    OPTIMA_DEMENTIA_DIR_TEST)
-        return dbank_control_test, dbank_dem_test, optima_control_test, optima_dem_test
-    elif dataset == 'all':
-        dbank_control_all  = read_file('dbank_control_all.pickle', DEMENTIABANK_CONTROL_DIR_ALL)
-        dbank_dem_all      = read_file('dbank_dem_all.pickle',     DEMENTIABANK_DEMENTIA_DIR_ALL)
-        optima_control_all = read_file('optima_control_all.pickle',OPTIMA_CONTROL_DIR_ALL)
-        optima_dem_all     = read_file('optima_dem_all.pickle',    OPTIMA_DEMENTIA_DIR_ALL)
-        return dbank_control_all, dbank_dem_all, optima_control_all, optima_dem_all
-    else:
-        print "Error in get_data(dataset): dataset must be one of 'all','train' or 'test'"
-        return 
-
-
 def get_all_book_pickles():
     wtow           = get_book_data('wtow.pickle',   WTOW_DIR)
     ijamob         = get_book_data('ijamob.pickle', IJAMOB_DIR)
     return wtow, ijamob
-
-def get_dbank_control():
-    return get_data('dbank_control.pickle',DEMENTIABANK_CONTROL_DIR_ALL)
-
-def get_dbank_dem():
-    return get_data('dbank_dem.pickle', DEMENTIABANK_DEMENTIA_DIR_ALL)
 
 # -------------End Of Get Data functions-----------
 
@@ -130,9 +102,9 @@ def extract_features(data,pickle_name = None, pickle_frequency = 20):
     feature_set = []
     # Load partial pickle if exists
     # We do this so if something crashes midrun we don't need to rerun the whole feature extractor
-    if pickle_name and os.path.exists(OUTPUT_DIR + pickle_name):
-        print "Partial pickle found at:", OUTPUT_DIR + pickle_name
-        feature_set = open_pickle(OUTPUT_DIR + pickle_name)
+    if pickle_name and os.path.exists(FEATURE_VEC_DIR + pickle_name):
+        print "Partial pickle found at:", FEATURE_VEC_DIR + pickle_name
+        feature_set = open_pickle(FEATURE_VEC_DIR + pickle_name)
 
     if len(feature_set) == len(data):
         print pickle_name, "features loaded"
@@ -151,115 +123,127 @@ def extract_features(data,pickle_name = None, pickle_frequency = 20):
             # Save every pickle_frequency'th interview 
             if pickle_name and idx % pickle_frequency == 0:
                 print "Saving interview feature vector up to:", len(feature_set)
-                overwrite_pickle(OUTPUT_DIR + pickle_name, feature_set)
+                overwrite_pickle(FEATURE_VEC_DIR + pickle_name, feature_set)
     return feature_set
 
 
-def make_feature_vec_pickles(dataset, picklename):
-    print "=========================="
+def make_feature_vec(picklename, dataset):
     print "Making feature vector for:", picklename
     feature_vecs = extract_features(dataset,picklename,10)
     # Make iid 
-    random.shuffle(feature_vecs)
-    overwrite_pickle(OUTPUT_DIR + picklename, feature_vecs)
+    shuffle(feature_vecs)
+    overwrite_pickle(FEATURE_VEC_DIR + picklename, feature_vecs)
     print picklename, "complete"
-    print "=========================="
+    print "--------------------------"
+    return feature_vecs
 
 # -------------End of extract feature methods -----------
 
-# ---------------- Get feature vectors -----------------#
+# ---------------- Make arff files -----------------#
 
-def get_clinical_feature_data():
-    optimal_dem = open_pickle(OUTPUT_DIR + "optima_dem_feature_vector.pickle")
-    labels = ['Dementia'] * len(optimal_dem)
-    optimal_con = open_pickle(OUTPUT_DIR + "optima_control_feature_vector.pickle")
-    labels.extend(["Control"] * len(optimal_con))
-    dementia_dem = open_pickle(OUTPUT_DIR + "dbank_dem_feature_vector.pickle")
-    labels.extend(["Dementia"] * len(dementia_dem))
-    dementia_con = open_pickle(OUTPUT_DIR + "dbank_control_feature_vector.pickle")
-    labels.extend(["Control"] * len(dementia_con))
-    data = []
-    data.extend(optimal_dem)
-    data.extend(optimal_con)
-    data.extend(dementia_dem)
-    data.extend(dementia_con)
+def get_attribute_from_variable(var):
+    if is_number(var):
+        return "numeric"
+    else:
+        return "str"
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def add_labels(data,label):
+    labels = [label] * len(data)
     data = zip(data, labels)
     return data
 
+def make_arff_file(samples, file_name):
+    arff_file_name = ARFF_DIR + file_name + ".arff"
 
-def get_optima_feature_data():
-    optimal_dem = open_pickle(OUTPUT_DIR + "optima_dem_feature_vector.pickle")
-    labels = ['Dementia'] * len(optimal_dem)
-
-    optimal_con = open_pickle(OUTPUT_DIR + "optima_control_feature_vector.pickle")
-    labels.extend(["Control"] * len(optimal_con))
-
-    data = []
-    data.extend(optimal_dem)
-    data.extend(optimal_con)
-    data = zip(data, labels)
-    return data
-
-
-def get_dementiabank_feature_data():
-    dementia_dem = open_pickle(OUTPUT_DIR + "dbank_dem_feature_vector.pickle")
-    labels = ["Dementia"] * len(dementia_dem)
-
-    dementia_con = open_pickle(OUTPUT_DIR + "dbank_control_feature_vector.pickle")
-    labels.extend(["Control"] * len(dementia_con))
-
-    data = []
-    data.extend(dementia_dem)
-    data.extend(dementia_con)
-    data = zip(data, labels)
-    return data
-
-
-def get_all_feature_data():
-    clinical_data = get_clinical_feature_data()
-    ijamob_data = open_pickle(OUTPUT_DIR + "ijamob.pickle")
-    wtow_data = open_pickle(OUTPUT_DIR + "wtow.pickle")
-    labels = ['Dementia'] * len(wtow_data)
-    labels.extend(['Control'] * len(ijamob_data))
-    non_clinical_data = []
-    non_clinical_data.extend(wtow_data)
-    non_clinical_data.extend(ijamob_data)
-    non_clinical_data = zip(non_clinical_data, labels)
-    data = []
-    data.extend(clinical_data)
-    data.extend(non_clinical_data)
-    return data
-
+    arff_file = open(arff_file_name, 'w+')
+    # Write the headers
+    # Write the relation
+    arff_file.write('@RELATION \"' + file_name + '\"\n\n')
+    # Assuming that all samples will have the same features
+    # Assuming that all sample features are iterated in the same order
+    shuffle(samples) # Randomize samples
+    data_unzipped = zip(*samples)
+    samples = list(data_unzipped[0])
+    labels = list(data_unzipped[1])
+    label_order = []
+    for k,v in samples[0].iteritems():
+        attribute_str = '@ATTRIBUTE '
+        key = str(k).strip().replace('\'', '')
+        attribute_str += '\'' + key + '\''+ ' ' + get_attribute_from_variable(v)
+        label_order.append(k)
+        arff_file.write(attribute_str + '\n')
+    arff_file.write('@ATTRIBUTE class {Control, Dementia} \n')
+    # Begin writing the data
+    arff_file.write('@DATA\n')
+    for sample in range(0, len(samples)):
+        data_str = ''
+        for k in label_order:
+            data_str += str(samples[sample][k]).strip() + ','
+        data_str += labels[sample]
+        arff_file.write(data_str + '\n')
+    arff_file.close()
 
 
 if __name__ == '__main__':
 
-    # # Check if feature vector pickles exist - if so use them, if not parse
-    # parser.parse(TEST_DIR)
-    dbank_control_train, dbank_dem_train, optima_control_train, optima_dem_train = get_data("train")
-    dbank_control_test,  dbank_dem_test,  optima_control_test,  optima_dem_test  = get_data("test")
-    # wtow, ijamob = get_all_book_pickles()
+    print ''' 
+        =============================
+        Getting raw text dictionaries
+        ============================= 
+    '''
+    dbank_control_train  = read_file('dbank_control_train.pickle', DEMENTIABANK_CONTROL_DIR_TRAIN)
+    dbank_control_test   = read_file('dbank_control_test.pickle',  DEMENTIABANK_CONTROL_DIR_TEST)
 
+    dbank_dem_train      = read_file('dbank_dem_train.pickle',     DEMENTIABANK_DEMENTIA_DIR_TRAIN)
+    dbank_dem_test       = read_file('dbank_dem_test.pickle',      DEMENTIABANK_DEMENTIA_DIR_TEST)
+
+    optima_control_train = read_file('optima_control_train.pickle',OPTIMA_CONTROL_DIR_TRAIN)
+    optima_control_test  = read_file('optima_control_test.pickle', OPTIMA_CONTROL_DIR_TEST)
+
+    optima_dem_train     = read_file('optima_dem_train.pickle',    OPTIMA_DEMENTIA_DIR_TRAIN)
+    optima_dem_test      = read_file('optima_dem_test.pickle',     OPTIMA_DEMENTIA_DIR_TEST)
+
+    print ''' 
+        =======================================
+        Turning into numerical feature vectors 
+        ======================================= 
+    '''
     # Load and pickle dbank_dem
-    make_feature_vec_pickles(dbank_dem_train,"dbank_dem_fv_train.pickle")
-    make_feature_vec_pickles(dbank_dem_test,"dbank_dem_fv_test.pickle")
+    dbank_dem_train_fv      = make_feature_vec("dbank_dem_fv_train.pickle", dbank_dem_train)
+    dbank_dem_test_fv       = make_feature_vec("dbank_dem_fv_test.pickle", dbank_dem_test)
     
     # Load and pickle dbank_control
-    make_feature_vec_pickles(dbank_control_train,"dbank_control_fv_train.pickle")
-    make_feature_vec_pickles(dbank_control_test,"dbank_control_fv_test.pickle")
+    dbank_control_train_fv  = make_feature_vec("dbank_control_fv_train.pickle", dbank_control_train)
+    dbank_control_test_fv   = make_feature_vec("dbank_control_fv_test.pickle", dbank_control_test)
     
     # Load and pickle optima_control
-    make_feature_vec_pickles(optima_control_train,"optima_control_fv_train.pickle")
-    make_feature_vec_pickles(optima_control_test,"optima_control_fv_test.pickle")
+    optima_control_train_fv = make_feature_vec("optima_control_fv_train.pickle", optima_control_train)
+    optima_control_test_fv  = make_feature_vec("optima_control_fv_test.pickle", optima_control_test)
     
     # Load and pickle optima_dem
-    make_feature_vec_pickles(optima_dem_train,"optima_dem_fv_train.pickle")
-    make_feature_vec_pickles(optima_dem_test,"optima_dem_fv_test.pickle")
+    optima_dem_train_fv     = make_feature_vec("optima_dem_fv_train.pickle", optima_dem_train)
+    optima_dem_test_fv      = make_feature_vec("optima_dem_fv_test.pickle", optima_dem_test)
+    
+    print ''' 
+        ==================
+        Making arff files 
+        ================== 
+    '''
+    make_arff_file(add_labels(dbank_dem_train_fv,"Dementia"),     "dbank_dem_train")
+    make_arff_file(add_labels(dbank_dem_test_fv, "Dementia"),     "dbank_dem_test")
+    make_arff_file(add_labels(dbank_control_train_fv,"Control"),  "dbank_control_train")
+    make_arff_file(add_labels(dbank_control_test_fv, "Control"),  "dbank_control_test")
+    make_arff_file(add_labels(optima_control_train_fv,"Control"), "optima_control_train")
+    make_arff_file(add_labels(optima_control_test_fv, "Control"), "optima_control_test")
+    make_arff_file(add_labels(optima_dem_train_fv,"Dementia"),    "optima_dem_train")
+    make_arff_file(add_labels(optima_dem_test_fv, "Dementia"),    "optima_dem_test")
 
-    # # Load and pickle wtow
-    # make_feature_vec_pickles(wtow, "wtow.pickle")
-
-    # # Load and pickle ijamob
-    # make_feature_vec_pickles(ijamob, "ijamob.pickle")
+    print "Done! Arff files located at: " + ARFF_DIR
 
